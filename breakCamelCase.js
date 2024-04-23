f079bcb77e843fb676c05bc0e6b65fb4164bb501
BerkovskiiAlex <berkovskii845@gmail.com>
2024-04-23 18:15:16 +0300

    breakCamelCase

---
+++ breakCamelCase.js
@@ -0,0 +1,13 @@
+/** @format */
+
+function solution(string) {
+  let result = "";
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i].toUpperCase()) {
+      result += " " + string[i];
+    } else {
+      result += string[i];
+    }
+  }
+  return result;
+}
