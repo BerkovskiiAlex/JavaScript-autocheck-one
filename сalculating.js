7a45bb99f64c356d7654551264ed8a1fac4fcf26
BerkovskiiAlex <berkovskii845@gmail.com>
2024-05-08 22:00:51 +0300

    add calculating

---
+++ сalculating.js
@@ -0,0 +1,58 @@
+/** @format */
+
+function zero(operation) {
+  return operation ? operation(0) : 0;
+}
+function one(operation) {
+  return operation ? operation(1) : 1;
+}
+function two(operation) {
+  return operation ? operation(2) : 2;
+}
+function three(operation) {
+  return operation ? operation(3) : 3;
+}
+function four(operation) {
+  return operation ? operation(4) : 4;
+}
+function five(operation) {
+  return operation ? operation(5) : 5;
+}
+function six(operation) {
+  return operation ? operation(6) : 6;
+}
+function seven(operation) {
+  return operation ? operation(7) : 7;
+}
+function eight(operation) {
+  return operation ? operation(8) : 8;
+}
+function nine(operation) {
+  return operation ? operation(9) : 9;
+}
+
+function plus(x) {
+  return function (y) {
+    return y + x;
+  };
+}
+function minus(x) {
+  return function (y) {
+    return y - x;
+  };
+}
+function times(x) {
+  return function (y) {
+    return y * x;
+  };
+}
+function dividedBy(x) {
+  return function (y) {
+    return Math.floor(y / x);
+  };
+}
+
+console.log(seven(times(five())));
+console.log(four(plus(nine())));
+console.log(eight(minus(three())));
+console.log(six(dividedBy(two())));
